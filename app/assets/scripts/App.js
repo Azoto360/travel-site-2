31233b5e66ca917583407d6e7846fd6c0c0760d5
Azoto <azoto360@gmail.com>
2022-09-21 21:45:56 +0200

    Deleted the examples

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,8 @@
 import '../styles/styles.css'
-import Person from './modules/Person'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept ()
 }
-
-var john = new Person("John Doe", "red");
-john.greet();
-
-var jane = new Person("Jane Smith", "green");
-jane.greet();
