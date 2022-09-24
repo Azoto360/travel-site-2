3f9443b7daa72c2738d5202554b93412ef570eee
Azoto <azoto360@gmail.com>
2022-09-24 18:19:26 +0200

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,26 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-    body: "Welcome to the super secret area"
-    })
+    const secretContent =`
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    `
+    let body 
 
-}
+    if(event.body) {
+        body = JSON.parse(event.body)
+    } else {}
+    }
+
+    if(body.password == "Pizza") {
+        callback(null, {
+            statusCode: 200,
+        body: secretContent
+        })
+    
+
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    
+
+    }
